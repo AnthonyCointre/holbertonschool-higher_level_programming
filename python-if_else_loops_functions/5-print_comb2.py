6124b8acbc3865bd2763f90959f6901ebd6285c2
AnthonyCointre <8732@holbertonstudents.com>
2024-05-08 10:28:30 +0200

    creation 5-print_comb2.py

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for number in range(0, 99):
-    print("{:02}, ".format(number), end="")
-print("{}".format(99))
+for i in range(100):
+    print("{:02d}".format(i), end=", " if i < 99 else "\n")
