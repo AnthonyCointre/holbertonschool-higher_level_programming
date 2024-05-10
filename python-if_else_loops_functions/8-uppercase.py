29cd308c9ad654146fcfff67e3b71f0bc1ab7546
AnthonyCointre <8732@holbertonstudents.com>
2024-05-10 08:50:29 +0200

    creation 8-uppercase.py

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def uppercase(string):
+    for character in string:
+        if 97 <= ord(character) <= 122:
+            print(chr(ord(character) - 32), end='')
+        else:
+            print(character, end='')
+    print()
