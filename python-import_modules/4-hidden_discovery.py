6976a7f078bcfbed7f4a903b75d8fa00e52d2e32
AnthonyCointre <8732@holbertonstudents.com>
2024-05-10 10:16:14 +0200

    creation 4-hidden_discovery.py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """ Import names from hidden_4.pyc """
     import hidden_4
-    names = []
-    for name in dir(hidden_4):
-        if not name[0:2] == "__":
-            names.append(name)
-    names.sort()
+    names = dir(hidden_4)
     for name in names:
-        print(name)
+        if name[:2] != "__":
+            print("{}".format(name))
