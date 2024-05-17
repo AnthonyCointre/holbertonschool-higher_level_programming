0a2eefce1aa425444a6ce1e1b3f2cb1df0767c21
AnthonyCointre <8732@holbertonstudents.com>
2024-05-17 09:58:10 +0200

    creation 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Class Square by 0-square.py"""
+
+
+class Square:
+    """
+    Represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
