928000aae61b4fa3b25e520572a780213b353bc2
AnthonyCointre <8732@holbertonstudents.com>
2024-05-17 09:59:15 +0200

    creation 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,5 +3,11 @@
 
 
 class Square:
+    """
+    Represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
     def __init__(self, size):
         self.__size = size
