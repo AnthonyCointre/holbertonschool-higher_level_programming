2a85916a4d9e37a8d1d69c1d1d94d318f069084d
AnthonyCointre <8732@holbertonstudents.com>
2024-05-21 17:17:55 +0200

    creation 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""def add_integer"""
+"""adds 2 integers"""
 
 
 def add_integer(a, b=98):
-    """adds 2 integers"""
-
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
+
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+
     return int(a) + int(b)
