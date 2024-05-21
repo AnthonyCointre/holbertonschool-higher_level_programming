b43e101311e4b0f7e79f49fa6c67e5ca3dcb7b80
AnthonyCointre <8732@holbertonstudents.com>
2024-05-21 11:29:51 +0200

    creation 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,10 +3,10 @@
 
 
 def add_integer(a, b=98):
-    """adds 2 integers"""
+	"""adds 2 integers"""
 
-    if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
-    return int(a) + int(b)
+	if not isinstance(a, (int, float)):
+		raise TypeError("a must be an integer")
+	if not isinstance(b, (int, float)):
+		raise TypeError("b must be an integer")
+	return int(a) + int(b)
