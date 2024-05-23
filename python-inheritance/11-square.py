dc3c8c77d9f0e0e6a1108697e0c83412a768917f
AnthonyCointre <8732@holbertonstudents.com>
2024-05-23 15:49:56 +0200

    creation 11-square.py

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -27,16 +27,6 @@ class Square(Rectangle):
 
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
 
-    def integer_validator(self, name, value):
-        """
-        Validate value.
-        """
-
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
     def area(self):
         """
         Calculate the area of the square.
