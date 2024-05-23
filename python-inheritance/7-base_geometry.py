c512e6462e1c4a90972d1760a63572f4fa39be6e
AnthonyCointre <8732@holbertonstudents.com>
2024-05-23 15:15:33 +0200

    creation 7-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -18,7 +18,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """
-        Validates value
+        Validates value.
         """
 
         if not isinstance(value, int):
