91f14e2903a1d647ca7ed6f0bcec3d946534a870
AnthonyCointre <8732@holbertonstudents.com>
2024-05-23 15:46:14 +0200

    creation 10-square.py

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -16,10 +16,23 @@ class Square(Rectangle):
         """
         Initializes a Square instance with a size.
         """
+
+        self.integer_validator("size", size)
         super().__init__(size, size)
 
     def __str__(self):
         """
         Returns a string representation of the square.
         """
+
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def integer_validator(self, name, value):
+        """
+        Validate value.
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
