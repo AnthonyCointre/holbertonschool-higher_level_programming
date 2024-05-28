0116c3f42a6e2572b455e0b579b4f4e1e7a1d9a2
AnthonyCointre <8732@holbertonstudents.com>
2024-05-28 09:41:03 +0200

    creation 10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -23,7 +23,6 @@ class Student():
         self.last_name = last_name
         self.age = age
 
-
     def to_json(self, attrs=None):
         """
         Method that returns the dictionary representation
