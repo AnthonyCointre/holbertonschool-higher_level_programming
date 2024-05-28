975e12bf7fa641d01f11c8fccb0185e6d2563648
AnthonyCointre <8732@holbertonstudents.com>
2024-05-28 09:38:04 +0200

    creation 10-student.py

---
+++ python-input_output/10-student.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+A class Student that defines a student by: (based on 9-student.py)
+
+Public instance attributes:
+first_name
+last_name
+age
+"""
+
+
+class Student():
+    """
+    A class Student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize the Student object.
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Method that returns the dictionary representation
+        of a Student instance.
+        """
+
+        dictionary = {}
+
+        if isinstance(attrs, list):
+            flag = True
+        else:
+            flag = False
+
+        for key, value in self.__dict__.items():
+            if flag:
+                for val in attrs:
+                    if val == key:
+                        dictionary[key] = value
+            else:
+                dictionary[key] = value
+
+        return (dictionary)
