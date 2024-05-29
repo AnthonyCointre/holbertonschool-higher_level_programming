fe6f6d35db98e96c5202a5ec0a47b9403d1b94d5
AnthonyCointre <8732@holbertonstudents.com>
2024-05-29 10:41:37 +0200

    creation task_00_basic_serialization.py

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -11,13 +11,13 @@ def serialize_and_save_to_file(data, filename):
     """
     Serialize a Python dictionary to a JSON file.
     """
-    with open(filename, mode="w", encoding="utf-8") as My_File:
-        json.dump(data, My_File)
+    with open(filename, mode="w", encoding="utf-8") as file:
+        json.dump(data, file)
 
 
 def load_and_deserialize(filename):
     """
     Deserialize a JSON file and recreate Python dictionary.
     """
-    with open(filename, mode="r", encoding="utf-8") as My_File:
-        return (json.load(My_File))
+    with open(filename, mode="r", encoding="utf-8") as file:
+        return (json.load(file))
