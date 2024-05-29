4fb28cfdb394d05abc6aa48b80d7cea75b2e81e0
AnthonyCointre <8732@holbertonstudents.com>
2024-05-29 10:59:07 +0200

    creation task_03_xml.py

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Serialization and deserialization using XML as an alternative format to JSON.
+"""
+import xml.etree.ElementTree as ET
+
+
+def serialize_to_xml(dictionary, filename):
+    """
+        Take a Python dictionary and a filename as parameters.
+        It should serialize the dictionary into XML
+        and save it to the given filename.
+    """
+
+    root = ET.Element('data')
+    for key, value in dictionary.items():
+        tree = ET.SubElement(root, key)
+        tree.text = value
+    tree = ET.ElementTree(root)
+    tree.write(filename, encoding='utf-8')
+
+
+def deserialize_from_xml(filename):
+    """
+        Take a filename as its parameter,
+        read the XML data from that file,
+        and return a deserialized Python dictionary.
+    """
+
+    dictionary = {}
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    for child in root:
+        dictionary[child.tag] = child.text
+    return (dictionary)
