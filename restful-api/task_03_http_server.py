c73608acc7833abb191e80535e72ccdd06ec4367
AnthonyCointre <8732@holbertonstudents.com>
2024-06-02 15:18:45 +0200

    creation task_03_http_server.py

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+Set up a basic web server using the http.server module.
+Handle different types of HTTP requests (GET, POST, etc.).
+Serve JSON data in response to specific endpoints.
+"""
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    """
+    A class SimpleHTTPRequestHandler.
+    """
+
+    def do_GET(self):
+        """
+        Handle GET requests.
+        """
+
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Hello, this is a simple API!')
+        elif self.path == '/data':
+            self.send_response(200)
+            self.send_header('content-type', 'application/json')
+            self.end_headers()
+            dataset = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(dataset).encode('UTF-8'))
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header('content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'OK')
+        elif self.path == '/info':
+            self.send_response(200)
+            self.send_header('content-type', 'application/json')
+            self.end_headers()
+            info = {"version": "1.0", "description":
+                    "A simple API built with http.server"}
+            self.wfile.write(json.dumps(info).encode('UTF-8'))
+        else:
+            self.send_response(404)
+            self.send_header('content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'404 Not Found')
