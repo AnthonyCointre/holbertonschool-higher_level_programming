2f0e52eae5d004ab1bd8f76b5024bef2e9cfdbfa
AnthonyCointre <8732@holbertonstudents.com>
2024-06-18 19:32:55 +0200

    creation 6-states.sql

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
--- create the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
+-- create the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on the MySQL server.
 
 CREATE TABLE IF NOT EXISTS states (
     id INT AUTO_INCREMENT PRIMARY KEY,
