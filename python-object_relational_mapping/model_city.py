0e2a69e6154ce104ab03a18913331a1bd4f6d5e3
AnthonyCointre <8732@holbertonstudents.com>
2024-06-20 18:51:26 +0200

    modification model_city.py

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -4,10 +4,8 @@ Contain the class definition of a City.
 """
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 from model_state import Base
-
-Base = declarative_base()
 
 
 class City(Base):
@@ -16,6 +14,7 @@ class City(Base):
     """
 
     __tablename__ = "cities"
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state = relationship("State", back_populates="cities")
