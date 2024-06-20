2df2e0ac7b674393dcf1f209b2abb4eaa250fe28
AnthonyCointre <8732@holbertonstudents.com>
2024-06-20 18:06:34 +0200

    creation 9-model_state_filter_a.py

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+list all State objects that contain the letter a
+from the database hbtn_0e_6_usa.
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def main():
+    if len(sys.argv) != 4:
+        print("""
+              Usage: {} <mysql username> <mysql password> <database name>
+              """.format(sys.argv[0])
+              )
+        sys.exit(1)
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    engine = create_engine(
+        f"mysql+mysqldb://{mysql_username}:{mysql_password}"
+        f"@localhost:3306/{database_name}"
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).filter(
+        State.name.like('%a%')).order_by(State.id).all()
+    for state in states:
+        print(f"{state.id}: {state.name}")
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
