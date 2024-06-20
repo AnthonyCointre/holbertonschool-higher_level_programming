a34a31fe2ea77236ed1be4e7dceca8c97ac11ee5
AnthonyCointre <8732@holbertonstudents.com>
2024-06-20 23:07:46 +0200

    modification 3-my_safe_filter_states.py

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,29 +6,35 @@ But this time, write one that is safe from MySQL injections
 """
 
 import MySQLdb
-import sys
+from sys import argv
 
 
 def main():
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name = sys.argv[4]
-    db = MySQLdb.connect(
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+    conn = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=mysql_username,
         passwd=mysql_password,
-        db=database_name
+        db=database_name,
+        charset="utf8"
     )
-    cursor = db.cursor()
-    query = "SELECT * FROM states WHERE BINARY name = %s ORDER BY id ASC;"
-    cursor.execute(query, (state_name,))
-    rows = cursor.fetchall()
-    for row in rows:
+    cur = conn.cursor()
+    cur.execute(
+        """
+        SELECT *
+        FROM states
+        WHERE name = %s
+        ORDER BY states.id ASC
+        """
+    )
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
-    cursor.close()
-    db.close()
+    cur.close()
+    conn.close()
 
 
 if __name__ == "__main__":
