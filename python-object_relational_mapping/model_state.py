dfc5e85ab25fbf8db0c7da86277f0a8b96ceff8a
AnthonyCointre <8732@holbertonstudents.com>
2024-06-20 23:08:34 +0200

    modification model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,10 +4,8 @@ Contain the class definition of a State and
 an instance Base = declarative_base().
 """
 
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-import sys
 
 Base = declarative_base()
 
@@ -18,23 +16,6 @@ class State(Base):
     """
 
     __tablename__ = "states"
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, autoincrement=True, unique=True,
+                nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-
-
-def main():
-    if len(sys.argv) != 4:
-        print("""
-              Usage:./model_state.py
-              <mysql username>
-              <mysql password>
-              <database name>
-              """
-              )
-        sys.exit(1)
-    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
-    engine = create_engine(
-        f"mysql+mysqldb://{username}:{password}"
-        f"@localhost/{db_name}", pool_pre_ping=True
-    )
-    Base.metadata.create_all(engine)
