057ea8aa51b2c64fc30c70c3a4692c6b691ca858
AnthonyCointre <8732@holbertonstudents.com>
2024-06-20 18:26:47 +0200

    modification model_city.py

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -7,6 +7,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from model_state import Base
 
+
 class City(Base):
     """
     A class City that links to the MySQL table cities.
