36bac7f8a24d8fcda9bbe0bf5d7cec09254fc1b8
AnthonyCointre <8732@holbertonstudents.com>
2024-06-20 23:10:26 +0200

    modification 10-model_state_my_get.py

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -4,34 +4,27 @@ Print the State object with the name passed as argument
 from the database hbtn_0e_6_usa.
 """
 
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sys import argv
 from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 
 def main():
-    if len(sys.argv) != 5:
-        print("""
-              Usage: {}
-              <mysql username>
-              <mysql password>
-              <database name>
-              <state name>
-              """.format(sys.argv[0])
-              )
-        sys.exit(1)
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name = sys.argv[4]
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+    state_name_to_search = argv[4]
     engine = create_engine(
-        f"mysql+mysqldb://{mysql_username}:{mysql_password}"
-        f"@localhost:3306/{database_name}"
+        "mysql+mysqldb://{}:{}@localhost/{}"
+        .format(mysql_username, mysql_password, database_name),
+        pool_pre_ping=True
     )
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).filter(State.name == state_name).first()
+    state = session.query(State).filter(
+        State.name == state_name_to_search).all()
     if state:
         print(state.id)
     else:
