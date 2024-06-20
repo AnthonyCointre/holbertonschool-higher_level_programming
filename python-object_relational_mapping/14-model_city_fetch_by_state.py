017aa49ad2465d80031f61a706a1e370dda9a82c
AnthonyCointre <8732@holbertonstudents.com>
2024-06-20 23:11:08 +0200

    modification 14-model_city_fetch_by_state.py

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -3,30 +3,23 @@
 Print all City objects from the database hbtn_0e_14_usa.
 """
 
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sys import argv
 from model_state import Base, State
 from model_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 
 def main():
-    if len(sys.argv) != 4:
-        print("""
-              Usage: {}
-              <mysql username>
-              <mysql password>
-              <database name>
-              """.format(sys.argv[0])
-              )
-        sys.exit(1)
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
     engine = create_engine(
-        f"mysql+mysqldb://{mysql_username}:{mysql_password}"
-        f"@localhost:3306/{database_name}"
+        "mysql+mysqldb://{}:{}@localhost/{}"
+        .format(mysql_username, mysql_password, database_name),
+        pool_pre_ping=True
     )
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     cities = session.query(City, State).filter(City.state_id == State.id).all()
