8be2331ffb4f7bb07de38520bc89c8e5ffe056b1
AnthonyCointre <8732@holbertonstudents.com>
2024-06-20 19:04:35 +0200

    modification 14-model_city_fetch_by_state.py

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -9,7 +9,6 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from model_city import City
 
-
 def main():
     if len(sys.argv) != 4:
         print("Usage: python 14-model_city_fetch_by_state.py <mysql_username> <mysql_password> <database_name>")
@@ -19,22 +18,16 @@ def main():
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
 
-    # Database connection
-    engine = create_engine(
-        f"mysql+mysqldb://{mysql_username}:{mysql_password}@localhost:3306/{database_name}")
+    engine = create_engine(f"mysql+mysqldb://{mysql_username}:{mysql_password}@localhost:3306/{database_name}")
 
-    # Bind the Base class to the engine
     Base.metadata.bind = engine
 
-    # Create session
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
 
     try:
-        # Query all cities, join with states, and order by city id
         cities = session.query(City).join(State).order_by(City.id).all()
 
-        # Print results in the required format
         for city in cities:
             print(f'{city.state.name}: ({city.id}) {city.name}')
 
@@ -44,7 +37,6 @@ def main():
 
     finally:
         session.close()
-
 
 if __name__ == '__main__':
     main()
