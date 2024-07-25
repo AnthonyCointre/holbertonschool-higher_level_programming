5a6c36bcfa12db99f1d513fea9342a50985b5ddd
AnthonyCointre <8732@holbertonstudents.com>
2024-07-25 14:21:58 +0200

    creation 7-script.js

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,19 @@
+fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+  .then(response => response.json())
+  .then(data => {
+    const movies = data.results;
+    const list = document.querySelector('#list_movies');
+
+    movies.forEach(movie => {
+      const listItem = document.createElement('li');
+      listItem.textContent = movie.title;
+      list.appendChild(listItem);
+    });
+  })
+  .catch(error => {
+    console.error('Error:', error);
+    const list = document.querySelector('#list_movies');
+    const listItem = document.createElement('li');
+    listItem.textContent = 'Failed to load movies';
+    list.appendChild(listItem);
+  });
