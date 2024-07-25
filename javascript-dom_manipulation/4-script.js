507f36118f8db271bf3fffffc9f84bc2ab76df0f
AnthonyCointre <8732@holbertonstudents.com>
2024-07-25 14:13:03 +0200

    creation 4-script.js

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,6 @@
+document.querySelector('#add_item').addEventListener('click', function() {
+  const ulElement = document.querySelector('.my_list');
+  const liElement = document.createElement('li');
+  liElement.textContent = 'Item';
+  ulElement.appendChild(liElement);
+});
