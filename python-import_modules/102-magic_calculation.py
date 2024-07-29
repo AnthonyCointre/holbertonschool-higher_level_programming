5eaeb86f1763f857932002a6cc5dc5307516f95f
AnthonyCointre <8732@holbertonstudents.com>
2024-07-29 10:21:23 +0200

    creation 102-magic_calculation.py

---
+++ python-import_modules/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    magic_calculation_102 = __import__('magic_calculation_102', fromlist=['add', 'sub'])
+    add = getattr(magic_calculation_102, 'add')
+    sub = getattr(magic_calculation_102, 'sub')
+
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
