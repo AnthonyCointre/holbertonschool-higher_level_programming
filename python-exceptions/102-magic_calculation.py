e9dfee49ea9b0f8546d088f2bc74a0bc0a68d149
AnthonyCointre <8732@holbertonstudents.com>
2024-07-29 10:53:06 +0200

    creation 102-magic_calculation.py

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
     result = 0
-    try:
-        for i in range(1, 4):
+    for i in range(1, 4):
+        try:
             if i > a:
                 raise Exception("Too far")
             result += (a ** b) / i
-    except Exception:
-        result = a + b
+        except Exception:
+            result = a + b
+            break
     return result
