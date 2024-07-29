bfa0ef4d6de3c04afbc245ab8b6fd072f9c9b358
AnthonyCointre <8732@holbertonstudents.com>
2024-07-29 10:22:14 +0200

    creation 102-magic_calculation.py

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    magic_calculation_102 = __import__('magic_calculation_102', fromlist=['add', 'sub'])
+    magic_calculation_102 = __import__(
+        'magic_calculation_102', fromlist=['add', 'sub'])
     add = getattr(magic_calculation_102, 'add')
     sub = getattr(magic_calculation_102, 'sub')
 
